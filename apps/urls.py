88e27d5478d39afb67ebeb429cefb9efd7bec3b7
lenovo <ibragimovmukhammad020@gmail.com>
2024-03-18 16:20:43 +0500

    Django Project

---
+++ apps/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from apps.views import index, index2
+
+urlpatterns = [
+    path('home', index, name='home'),
+    path('about', index2, name='index2')
+]
---
+++ apps/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'home.html')
+
+def index2(request):
+    return render(request, 'about.html')
